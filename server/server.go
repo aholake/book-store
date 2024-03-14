c1400c5b63ec683ee0ec7ff7173b52fd8aa9e57c
Loc Vo <lvo@axon.com>
2024-03-14 14:49:56 +0700

    try to use 3rd proto file

--- server/server.go
+++ server/server.go
@@ -2,12 +2,14 @@ package server
 
 import (
 	"context"
+	"time"
 
 	"github.com/aholake/book-store/model"
 	pb "github.com/aholake/book-store/proto"
 	"github.com/aholake/book-store/repo"
 	"github.com/aholake/book-store/service"
 	"github.com/aholake/book-store/util"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Server struct {
@@ -35,9 +37,10 @@ func (s Server) Get(ctx context.Context, id *pb.BookIdRequest) (*pb.Book, error)
 		return nil, nil
 	}
 	return &pb.Book{
-		Id:     book.Id,
-		Title:  book.Title,
-		Author: book.Author,
+		Id:          book.Id,
+		Title:       book.Title,
+		Author:      book.Author,
+		PublishDate: timestamppb.Now(),
 	}, nil
 }
 
@@ -51,4 +54,16 @@ func (s Server) GetAll(context.Context, *pb.EmptyMessage) (*pb.GetAllResponse, e
 	return &pb.GetAllResponse{
 		Books: response,
 	}, nil
+}
+
+func (s Server) StreamingBook(request *pb.EmptyMessage, response pb.BookService_StreamingBookServer) error {
+	books := service.FindAll()
+	for _, b := range books {
+		resp := util.Convert(b)
+		if err := response.Send(&resp); err != nil {
+			return err
+		}
+		time.Sleep(time.Second)
+	}
+	return nil
 }
